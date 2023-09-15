01494929f6f53c56c0dc8d811d44140e1f6d8093
myawnaymalsaung <137532687+myawnaymalsaung@users.noreply.github.com>
2023-09-15 10:26:00 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,8 +13,8 @@ cyan='\e[96m'   #စိမ်းပြာရောင်
 none='\e[0m'    #အရောင်မရှိ
 
 #Username နှင့် Password ပြောင်းရန်
-username="iam404"
-password="iam404"
+username="myawm"
+password="myawm"
 
 #SSH USER LIMIT သတ်မှတ်ရန်
 sshlimiter="300"
@@ -31,7 +31,7 @@ servermessage="<h3><font color='red'>
 ▬▬▬▬▬▬▬▬✿4▪0▪4✿▬▬▬▬▬▬▬▬
 </font></h3>
 <h4><font color='cyan'>
-↤↤↤↤↤ GCP SERVER BY ④ ⍤⃝✿④ ↦↦↦↦↦
+↤↤↤↤↤ အိပ်ချင်သေးတယ် ↦↦↦↦↦
 </font></h4>
 <h3><font color='red'>
 ▬▬▬▬▬▬▬▬✿4▪0▪4✿▬▬▬▬▬▬▬▬
